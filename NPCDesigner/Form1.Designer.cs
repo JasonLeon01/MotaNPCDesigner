103bff25d5a45560f73cc986e0a7525edc7ce539
Jason Leon <jasonleon01@outlook.com>
2023-10-25 18:19:58 +0800

    update

--- NPCDesigner/Form1.Designer.cs
+++ NPCDesigner/Form1.Designer.cs
@@ -49,191 +49,184 @@
             checkBox1 = new CheckBox();
             checkBox2 = new CheckBox();
             listBox2 = new ListBox();
+            button7 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(15, 294);
-            button1.Margin = new Padding(4);
+            button1.Location = new Point(12, 250);
             button1.Name = "button1";
-            button1.Size = new Size(108, 29);
+            button1.Size = new Size(84, 25);
             button1.TabIndex = 0;
-            button1.Text = "新建文件";
+            button1.Text = "新建对话";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // button2
             // 
-            button2.Location = new Point(15, 331);
-            button2.Margin = new Padding(4);
+            button2.Location = new Point(12, 281);
             button2.Name = "button2";
-            button2.Size = new Size(108, 28);
+            button2.Size = new Size(84, 24);
             button2.TabIndex = 1;
-            button2.Text = "保存文件";
+            button2.Text = "删除对话";
             button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // listBox1
             // 
             listBox1.FormattingEnabled = true;
-            listBox1.ItemHeight = 20;
-            listBox1.Location = new Point(15, 13);
-            listBox1.Margin = new Padding(4);
+            listBox1.ItemHeight = 17;
+            listBox1.Location = new Point(12, 11);
             listBox1.Name = "listBox1";
-            listBox1.Size = new Size(108, 264);
+            listBox1.Size = new Size(85, 225);
             listBox1.TabIndex = 2;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
             // 
             // textBox1
             // 
             textBox1.Font = new Font("Microsoft YaHei UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox1.Location = new Point(274, 92);
-            textBox1.Margin = new Padding(4);
+            textBox1.Location = new Point(213, 78);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(336, 206);
+            textBox1.Size = new Size(262, 176);
             textBox1.TabIndex = 3;
+            textBox1.TextChanged += textBox1_TextChanged;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(501, 20);
-            textBox2.Margin = new Padding(4);
+            textBox2.Location = new Point(390, 17);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(84, 27);
+            textBox2.Size = new Size(66, 23);
             textBox2.TabIndex = 4;
             textBox2.Text = "0";
+            textBox2.TextChanged += textBox2_TextChanged;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(369, 58);
-            textBox3.Margin = new Padding(4);
+            textBox3.Location = new Point(287, 49);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(126, 27);
+            textBox3.Size = new Size(99, 23);
             textBox3.TabIndex = 5;
+            textBox3.TextChanged += textBox3_TextChanged;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(274, 22);
-            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Location = new Point(213, 19);
             label1.Name = "label1";
-            label1.Size = new Size(69, 20);
+            label1.Size = new Size(56, 17);
             label1.TabIndex = 6;
             label1.Text = "对话事件";
             // 
             // button3
             // 
-            button3.Location = new Point(354, 18);
-            button3.Margin = new Padding(4);
+            button3.Location = new Point(275, 15);
             button3.Name = "button3";
-            button3.Size = new Size(60, 29);
+            button3.Size = new Size(47, 25);
             button3.TabIndex = 7;
             button3.Text = "主角";
             button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
             // 
             // button4
             // 
-            button4.Location = new Point(422, 18);
-            button4.Margin = new Padding(4);
+            button4.Location = new Point(328, 15);
             button4.Name = "button4";
-            button4.Size = new Size(72, 29);
+            button4.Size = new Size(56, 25);
             button4.TabIndex = 8;
             button4.Text = "本事件";
             button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
             // 
             // label2
             // 
-            label2.Location = new Point(594, 14);
-            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Location = new Point(462, 12);
             label2.Name = "label2";
-            label2.Size = new Size(123, 46);
+            label2.Size = new Size(96, 39);
             label2.TabIndex = 9;
             label2.Text = "有其他确定事件号的在此处修改";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(274, 65);
-            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Location = new Point(213, 55);
             label3.Name = "label3";
-            label3.Size = new Size(84, 20);
+            label3.Size = new Size(68, 17);
             label3.TabIndex = 10;
             label3.Text = "对话人名称";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(504, 61);
-            label4.Margin = new Padding(4, 0, 4, 0);
+            label4.Location = new Point(392, 52);
             label4.Name = "label4";
-            label4.Size = new Size(84, 20);
+            label4.Size = new Size(68, 17);
             label4.TabIndex = 11;
             label4.Text = "此处可为空";
             // 
             // button5
             // 
-            button5.Location = new Point(149, 294);
-            button5.Margin = new Padding(4);
+            button5.Location = new Point(116, 250);
             button5.Name = "button5";
-            button5.Size = new Size(100, 29);
+            button5.Size = new Size(78, 25);
             button5.TabIndex = 12;
             button5.Text = "增添";
             button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
             // 
             // button6
             // 
-            button6.Location = new Point(149, 331);
-            button6.Margin = new Padding(4);
+            button6.Location = new Point(116, 281);
             button6.Name = "button6";
-            button6.Size = new Size(100, 29);
+            button6.Size = new Size(78, 25);
             button6.TabIndex = 13;
             button6.Text = "删除";
             button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(274, 307);
-            label5.Margin = new Padding(4, 0, 4, 0);
+            label5.Location = new Point(213, 261);
             label5.Name = "label5";
-            label5.Size = new Size(129, 20);
+            label5.Size = new Size(104, 17);
             label5.TabIndex = 14;
             label5.Text = "结束后转换的名称";
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(415, 304);
-            textBox4.Margin = new Padding(4);
+            textBox4.Location = new Point(323, 258);
             textBox4.Name = "textBox4";
-            textBox4.Size = new Size(99, 27);
+            textBox4.Size = new Size(78, 23);
             textBox4.TabIndex = 15;
             textBox4.TextChanged += textBox4_TextChanged;
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(523, 307);
-            label6.Margin = new Padding(4, 0, 4, 0);
+            label6.Location = new Point(407, 261);
             label6.Name = "label6";
-            label6.Size = new Size(84, 20);
+            label6.Size = new Size(68, 17);
             label6.TabIndex = 16;
             label6.Text = "此处可为空";
             // 
             // checkBox1
             // 
             checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(618, 92);
-            checkBox1.Margin = new Padding(4);
+            checkBox1.Location = new Point(481, 78);
             checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(91, 24);
+            checkBox1.Size = new Size(75, 21);
             checkBox1.TabIndex = 17;
             checkBox1.Text = "是否淡出";
             checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
             // 
             // checkBox2
             // 
             checkBox2.AutoSize = true;
-            checkBox2.Location = new Point(274, 335);
-            checkBox2.Margin = new Padding(4);
+            checkBox2.Location = new Point(213, 285);
             checkBox2.Name = "checkBox2";
-            checkBox2.Size = new Size(136, 24);
+            checkBox2.Size = new Size(111, 21);
             checkBox2.TabIndex = 18;
             checkBox2.Text = "转换后立刻执行";
             checkBox2.UseVisualStyleBackColor = true;
@@ -242,18 +235,28 @@
             // listBox2
             // 
             listBox2.FormattingEnabled = true;
-            listBox2.ItemHeight = 20;
-            listBox2.Location = new Point(141, 13);
-            listBox2.Margin = new Padding(4);
+            listBox2.ItemHeight = 17;
+            listBox2.Location = new Point(110, 11);
             listBox2.Name = "listBox2";
-            listBox2.Size = new Size(108, 264);
+            listBox2.Size = new Size(85, 225);
             listBox2.TabIndex = 19;
+            listBox2.SelectedIndexChanged += listBox2_SelectedIndexChanged;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(323, 285);
+            button7.Name = "button7";
+            button7.Size = new Size(150, 25);
+            button7.TabIndex = 20;
+            button7.Text = "保存文件";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(9F, 20F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(720, 379);
+            AutoScaleMode = AutoScaleMode.None;
+            ClientSize = new Size(560, 322);
+            Controls.Add(button7);
             Controls.Add(listBox2);
             Controls.Add(checkBox2);
             Controls.Add(checkBox1);
@@ -276,7 +279,6 @@
             Controls.Add(button1);
             FormBorderStyle = FormBorderStyle.FixedDialog;
             Icon = (Icon)resources.GetObject("$this.Icon");
-            Margin = new Padding(4);
             MaximizeBox = false;
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
@@ -308,5 +310,6 @@
         private CheckBox checkBox1;
         private CheckBox checkBox2;
         private ListBox listBox2;
+        private Button button7;
     }
 }
--- NPCDesigner/Form1.cs
+++ NPCDesigner/Form1.cs
@@ -1,28 +1,235 @@
+using System.Text;
+
 namespace NPCDesigner
 {
     public partial class Form1 : Form
     {
+        List<NPC> npc;
+        int list1Index, list2Index;
         public Form1()
         {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            list1Index = 0;
+            list2Index = 0;
+            npc = new List<NPC>();
             InitializeComponent();
         }
         public void Form1_load(object sender, EventArgs e)
         {
-
+            int idx = 0;
+            string path = Application.StartupPath + @"data\npc\";
+            while (File.Exists(path + @"npc_" + idx.ToString() + ".dat"))
+            {
+                string file = @"npc_" + idx.ToString() + ".dat";
+                string datatext = System.IO.File.ReadAllText(path + file);
+                string[] data = datatext.Split(Environment.NewLine.ToCharArray());
+                data = data.Where(s => !string.IsNullOrEmpty(s)).ToArray();
+                listBox1.Items.Add(idx.ToString().PadLeft(3, '0') + "：" + file);
+                List<int> id = data[1].Split(':')[1].Split(",").Select(int.Parse).ToList();
+                List<string> name = data[2].Split(':')[1].Split(",").ToList();
+                List<string> message = data[3].Split(':')[1].Replace("\\n", "\r\n").Split(",").ToList();
+                List<(int, string, string)> temp = new List<(int, string, string)>();
+                for (int i = 0; i < id.Count; i++)
+                {
+                    temp.Add((id[i], name[i], message[i]));
+                    listBox2.Items.Add(name[i]);
+                }
+                npc.Add(new NPC(temp, data[4].Split(':')[1].Replace("none", ""), Convert.ToBoolean(int.Parse(data[5].Split(':')[1])), Convert.ToBoolean(int.Parse(data[6].Split(':')[1]))));
+                ++idx;
+            }
+            if (npc.Count == 0)
+            {
+                MessageBox.Show("未检测到NPC数据！");
+                Application.Exit();
+                return;
+            }
+            listBox1.SelectedIndex = 0;
+            listBox2.SelectedIndex = 0;
+            textBox2.Text = npc[0].info[0].id.ToString();
+            textBox3.Text = npc[0].info[0].name;
+            textBox1.Text = npc[0].info[0].message;
+            textBox4.Text = npc[0].transName;
+            checkBox1.Checked = npc[0].fade;
+            checkBox2.Checked = npc[0].directlyFunction;
         }
         private void checkBox2_CheckedChanged(object sender, EventArgs e)
         {
-            if (textBox4.Text == "" && checkBox2.Checked)
+            if (textBox4.Text == "" || textBox4.Text == "none" && checkBox2.Checked)
             {
                 MessageBox.Show("未设置转换名称，无法勾选");
+                npc[listBox1.SelectedIndex].directlyFunction = false;
                 checkBox2.Checked = false;
                 return;
             }
+            npc[listBox1.SelectedIndex].directlyFunction = checkBox2.Checked;
         }
         private void textBox4_TextChanged(object sender, EventArgs e)
         {
+            npc[listBox1.SelectedIndex].transName = textBox4.Text;
             if (textBox4.Text == "")
+            {
+                npc[listBox1.SelectedIndex].directlyFunction = false;
                 checkBox2.Checked = false;
+            }
+            if (textBox4.Text == "none")
+            {
+                MessageBox.Show("none和空白等同，将会变回空白，如有更多设置请更改这段源码");
+                textBox4.Text = "";
+                npc[listBox1.SelectedIndex].directlyFunction = false;
+                checkBox2.Checked = false;
+            }
         }
+        private void refreshList1()
+        {
+            listBox1.Items.Clear();
+            int idx = 0;
+            foreach (NPC n in npc)
+            {
+                listBox1.Items.Add(idx.ToString().PadLeft(3, '0') + "：npc_" + idx.ToString() + ".dat");
+                idx++;
+            }
+            listBox1.SelectedIndex = list1Index;
+        }
+        private void refreshList2()
+        {
+            listBox2.Items.Clear();
+            foreach (var v in npc[listBox1.SelectedIndex].info)
+                listBox2.Items.Add(v.name);
+            listBox2.SelectedIndex = list2Index;
+        }
+        private void updateMessage()
+        {
+            textBox2.Text = npc[listBox1.SelectedIndex].info[listBox2.SelectedIndex].id.ToString();
+            textBox3.Text = npc[listBox1.SelectedIndex].info[listBox2.SelectedIndex].name;
+            textBox1.Text = npc[listBox1.SelectedIndex].info[listBox2.SelectedIndex].message;
+            textBox4.Text = npc[listBox1.SelectedIndex].transName;
+            checkBox1.Checked = npc[listBox1.SelectedIndex].fade;
+            checkBox2.Checked = npc[listBox1.SelectedIndex].directlyFunction;
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            list1Index = listBox1.SelectedIndex;
+            listBox2.Items.Clear();
+            foreach (var info in npc[listBox1.SelectedIndex].info)
+                listBox2.Items.Add(info.name);
+            listBox2.SelectedIndex = 0;
+            list2Index = 0;
+            updateMessage();
+        }
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            list2Index = listBox2.SelectedIndex;
+            updateMessage();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            npc[listBox1.SelectedIndex].info[listBox2.SelectedIndex] = (-1, npc[listBox1.SelectedIndex].info[listBox2.SelectedIndex].name, npc[listBox1.SelectedIndex].info[listBox2.SelectedIndex].message);
+            updateMessage();
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            npc[listBox1.SelectedIndex].info[listBox2.SelectedIndex] = (-2, npc[listBox1.SelectedIndex].info[listBox2.SelectedIndex].name, npc[listBox1.SelectedIndex].info[listBox2.SelectedIndex].message);
+            updateMessage();
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            npc[listBox1.SelectedIndex].info[listBox2.SelectedIndex] = (int.Parse(textBox2.Text), npc[listBox1.SelectedIndex].info[listBox2.SelectedIndex].name, npc[listBox1.SelectedIndex].info[listBox2.SelectedIndex].message);
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            npc[listBox1.SelectedIndex].info[listBox2.SelectedIndex] = (npc[listBox1.SelectedIndex].info[listBox2.SelectedIndex].id, textBox3.Text, npc[listBox1.SelectedIndex].info[listBox2.SelectedIndex].message);
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            npc[listBox1.SelectedIndex].fade = checkBox1.Checked;
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            npc[listBox1.SelectedIndex].info.Add((0, "name", ""));
+            refreshList2();
+            updateMessage();
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (npc[listBox1.SelectedIndex].info.Count == 1)
+            {
+                MessageBox.Show("不允许删除最后一段对话");
+                return;
+            }
+            npc[listBox1.SelectedIndex].info.RemoveAt(listBox2.SelectedIndex);
+            if (list2Index >= npc[listBox1.SelectedIndex].info.Count) list2Index = npc[listBox1.SelectedIndex].info.Count - 1;
+            refreshList2();
+            updateMessage();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            npc.Add(new NPC(new List<(int, string, string)> { (0, "name", "") }, "", false, false));
+            refreshList1();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (npc.Count == 1)
+            {
+                MessageBox.Show("不允许删除最后剩余的文件");
+                return;
+            }
+            npc.RemoveAt(listBox1.SelectedIndex);
+            if (list1Index >= npc.Count) list1Index = npc.Count - 1;
+            refreshList1();
+            refreshList2();
+            updateMessage();
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            npc[listBox1.SelectedIndex].info[listBox2.SelectedIndex] = (npc[listBox1.SelectedIndex].info[listBox2.SelectedIndex].id, npc[listBox1.SelectedIndex].info[listBox2.SelectedIndex].name, textBox1.Text);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int idx = 0;
+            string file = Application.StartupPath + @"data\npc\";
+            foreach (NPC n in npc)
+            {
+                string data = "[npc]\n";
+                List<string> id = new List<string>();
+                List<string> name = new List<string>();
+                List<string> message = new List<string>();
+                for (int i = 0; i < n.info.Count; i++)
+                {
+                    id.Add(n.info[i].id.ToString());
+                    name.Add(n.info[i].name == "" ? "none" : n.info[i].name);
+                    message.Add(n.info[i].message == "" ? "none" : n.info[i].message);
+                }
+                data = data + "ID:" + string.Join(",", id) + "\n";
+                data = data + "name:" + string.Join(",", name) + "\n";
+                data = data + "message:" + string.Join(",", message).Replace("\r\n", "\\n") + "\n";
+                data = data + "transName:" + (n.transName == "" ? "none" : n.transName) + "\n";
+                data = data + "fade:" + Convert.ToInt32(n.fade).ToString() + "\n";
+                data = data + "directFunc:" + Convert.ToInt32(n.directlyFunction).ToString() + "\n";
+                System.IO.File.WriteAllText(file + @"npc_" + idx.ToString() + ".dat", data);
+                idx++;
+            }
+            while (File.Exists(file + @"npc_" + idx.ToString() + ".dat"))
+            {
+                System.IO.File.Delete(file + @"npc_" + (idx++).ToString() + ".dat");
+            }
+            MessageBox.Show("保存成功！");
+            refreshList2();
+            return;
+        }
+    }
+}
+
+class NPC
+{
+    public List<(int id, string name, string message)> info;
+    public string transName;
+    public bool fade, directlyFunction;
+    public NPC(List<(int, string, string)> info, string transName, bool fade, bool directlyFunction)
+    {
+        this.info = info;
+        this.transName = transName;
+        this.fade = fade;
+        this.directlyFunction = directlyFunction;
     }
 }
