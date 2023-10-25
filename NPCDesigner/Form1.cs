91db1f5725d92e6d0d52bf1810e5d5ce5d79ea0c
Jason Leon <jasonleon01@outlook.com>
2023-10-25 11:50:59 +0800

    update

--- NPCDesigner/Form1.cs
+++ NPCDesigner/Form1.cs
@@ -6,7 +6,10 @@ namespace NPCDesigner
         {
             InitializeComponent();
         }
+        public void Form1_load(object sender, EventArgs e)
+        {
 
+        }
         private void checkBox2_CheckedChanged(object sender, EventArgs e)
         {
             if (textBox4.Text == "" && checkBox2.Checked)
@@ -16,10 +19,9 @@ namespace NPCDesigner
                 return;
             }
         }
-
         private void textBox4_TextChanged(object sender, EventArgs e)
         {
-            if (textBox4.Text == "") 
+            if (textBox4.Text == "")
                 checkBox2.Checked = false;
         }
     }
